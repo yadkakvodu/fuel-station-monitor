ff3259bdd3312c52fe1fe50946ff8d943d8b7954
dinislamalipkachev <dalipkachev@inbox.ru>
2026-09-19 00:17:50 +0300

    first commit

---
+++ src/main/java/com/example/fuelstation/FuelStationMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.example.fuelstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelStationMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelStationMonitorApplication.class, args);
+	}
+
+}
